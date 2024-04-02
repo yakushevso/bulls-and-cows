f8d5e486437e0af22f0d9304c2c477f15bc93eb1
Home <yakushevso@ya.ru>
2024-04-02 21:16:30 +0300

    Stage 4/7: Game time!

--- Bulls and Cows/task/src/bullscows/Bullscows.java
+++ Bulls and Cows/task/src/bullscows/Bullscows.java
@@ -1,9 +1,79 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Bullscows {
+    private String secret;
+    private int bulls;
+    private int cows;
+
+    public Bullscows() {
+        bulls = 0;
+        cows = 0;
+    }
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter the secret code's length:");
+
+        int size;
+
+        while (true) {
+            size = sc.nextInt();
+
+            if (size < 10) {
+                break;
+            } else {
+                System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.\n", size);
+            }
+        }
+
+        randomSecretCode(size);
+        System.out.println("Okay, let's start a game!");
+
+        int i = 1;
+
+        while (bulls != secret.length()) {
+            bulls = 0;
+            cows = 0;
+            System.out.printf("Turn %d:\n", i++);
+            grader(sc.next());
+            printGrader();
+        }
+
+        System.out.println("Congratulations! You guessed the secret code.");
+    }
+
+    public void grader(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < secret.length(); j++) {
+                if (s.charAt(i) == secret.charAt(i)) {
+                    bulls++;
+                    break;
+                } else if (s.charAt(i) == secret.charAt(j)) {
+                    cows++;
+                }
+            }
+        }
+    }
+
+    public void printGrader() {
+        String bull = bulls == 1 ? "bull" : "bulls";
+        String cow = cows == 1 ? "cow" : "cows";
+
+        if (bulls > 0 && cows > 0) {
+            System.out.printf("Grade: %d %s and %d %s\n", bulls, bull, cows, cow);
+        } else if (bulls > 0) {
+            System.out.printf("Grade: %d %s\n", bulls, bull);
+        } else if (cows > 0) {
+            System.out.printf("Grade: %d %s\n", cows, cow);
+        } else {
+            System.out.println("Grade: None");
+        }
+    }
+
     public void randomSecretCode(int size) {
         if (size < 10) {
-            String secretCode;
 
             boolean check;
             do {
@@ -15,21 +85,22 @@ public class Bullscows {
                     stringBuilder.deleteCharAt(0);
                 }
 
-                secretCode = stringBuilder.substring(0, size);
+                secret = stringBuilder.substring(0, size);
+
                 check = false;
                 boolean[] digits = new boolean[10];
 
-                for (int i = 0; i < secretCode.length(); i++) {
-                    int digit = Character.getNumericValue(secretCode.charAt(i));
+                for (int i = 0; i < secret.length(); i++) {
+                    int digit = Character.getNumericValue(secret.charAt(i));
+
                     if (digits[digit]) {
                         check = true;
                     }
+
                     digits[digit] = true;
                 }
 
             } while (check);
-
-            System.out.printf("The random secret number is %s.", secretCode);
         } else {
             System.out.printf("Error: can't generate a secret number with a length of %d because there aren't enough unique digits.", size);
         }
--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,12 +1,8 @@
 package bullscows;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            Bullscows bullscows = new Bullscows();
-            bullscows.randomSecretCode(sc.nextInt());
-        }
+        Bullscows bullscows = new Bullscows();
+        bullscows.run();
     }
 }
