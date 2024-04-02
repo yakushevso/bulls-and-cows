90b3fffb4595abdb319790d3a487209b006ed148
Home <yakushevso@ya.ru>
2024-04-02 21:07:46 +0300

    Stage 2/7: Grader

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,17 +1,54 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
+    private final static int SECRET_CODE = 9305;
+
     public static void main(String[] args) {
-        System.out.println("""
-                The secret code is prepared: ****.
-                                
-                Turn 1. Answer:
-                1234
-                Grade: None.
-                                
-                Turn 2. Answer:
-                9876
-                Grade: 4 bulls.
-                Congrats! The secret code is 9876.""");
+        try (Scanner sc = new Scanner(System.in)) {
+            grader(sc.nextInt());
+        }
+    }
+
+    public static void grader(int n) {
+        int bulls = 0;
+        int cows = 0;
+
+        int[] input = splitToArray(n);
+        int[] secret = splitToArray(SECRET_CODE);
+
+        for (int i = 0; i < input.length; i++) {
+            for (int k : secret) {
+                if (input[i] == secret[i]) {
+                    bulls++;
+                    break;
+                } else if (input[i] == k) {
+                    cows++;
+                }
+            }
+        }
+
+        if (bulls > 0 && cows > 0) {
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %d.", bulls, cows, SECRET_CODE);
+        } else if (bulls > 0) {
+            System.out.printf("Grade: %d bull(s). The secret code is %d.", bulls, SECRET_CODE);
+        } else if (cows > 0) {
+            System.out.printf("Grade: %d cow(s). The secret code is %d.", cows, SECRET_CODE);
+        } else {
+            System.out.printf("Grade: None. The secret code is %d.", SECRET_CODE);
+        }
+    }
+
+    public static int[] splitToArray(int num) {
+        int count = String.valueOf(num).length();
+        int[] res = new int[count];
+
+        for (int i = count - 1; i >= 0; i--) {
+            res[i] = num % 10;
+            num /= 10;
+        }
+
+        return res;
     }
 }
--- Bulls and Cows/task/test/BullsCowsTest.java
+++ Bulls and Cows/task/test/BullsCowsTest.java
@@ -1,74 +1,162 @@
 import bullscows.Main;
-import org.hyperskill.hstest.dynamic.input.DynamicTestingMethod;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
+import org.hyperskill.hstest.testcase.TestCase;
 import org.hyperskill.hstest.testing.TestedProgram;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class BullsCowsTest extends StageTest<String> {
-    @DynamicTestingMethod
-    CheckResult test() {
+
+    @Override
+    public List<TestCase<String>> generate() {
+        return Arrays.asList(
+                new TestCase<String>().setDynamicTesting(this::test1),
+                new TestCase<String>().setDynamicTesting(this::test2),
+                new TestCase<String>().setDynamicTesting(this::test3));
+    }
+
+    String secretNumber;
+
+    // base test
+    CheckResult test1() {
         TestedProgram main = new TestedProgram();
+        main.start();
+        String input = "3601";
+        String output = main.execute(input);
+        return solver(input, output);
+    }
 
-        boolean turn = false, answer = false;
-        int turnsNumber = 0;
+    // test with 4 bulls
+    CheckResult test2() {
+        TestedProgram main = new TestedProgram();
+        main.start();
+        String output = main.execute(secretNumber);
 
-        String output = main.start(); // got a game log
-        String[] reply = output.split("\n");
+        return solver(secretNumber, output);
+    }
 
-        if (reply.length == 0) {
-            return CheckResult.wrong("Your output should not be empty!");
+    // test of None result
+    CheckResult test3() {
+        TestedProgram main = new TestedProgram();
+        main.start();
+        List<Integer> source = stringToArrayOfNumbers("1234567890");
+        List<Integer> secretNumberList = stringToArrayOfNumbers(secretNumber);
+        source.removeAll(secretNumberList);
+        String input = source.stream().map(String::valueOf).collect(Collectors.joining()).substring(0, 4);
+        String output = main.execute(input);
+        return solver(input, output);
+    }
+
+    CheckResult solver(String input, String output) {
+
+        if (!findPairsOfBullsAndCows(output)) {
+            return CheckResult.wrong(
+                    "The testing system didn't find a pairs of " +
+                            "bulls and cows or None in your program's output.");
         }
 
-        // started verifying process
-        String item = reply[0];
-        if (!item.contains("****") || item.contains("*****")) {
-            return CheckResult.wrong("The first line has an incorrect format.");
+        Matcher matcher = getFourDigitsMatcher(output);
+        if (!findFourDigitsWithRegExp(matcher)) {
+            return CheckResult.wrong(
+                    "The testing system didn't find a \"secret\" " +
+                            "number in your program's output.");
         }
 
-        for (int n = 1; n < reply.length - 1; n++) {
-            if (reply[n].toLowerCase().startsWith("turn")) {
-                if (turn || answer) {
-                    return CheckResult.wrong("The order of the steps in the game is broken 1.");
-                }
-                turn = true;
+        secretNumber = getFourDigits(matcher);
+        if (secretNumber.equals("9305")) {
+            System.out.println("Why 9305? Make your own secret number :)");
+        }
 
-            } else if (findFourDigitsWithRegExp(reply[n])) {
-                if (!turn || answer) {
-                    return CheckResult.wrong("The order of the steps in the game is broken 2.");
-                }
-                answer = true;
-            } else if (findPairsOfBullsAndCows(reply[n])) {
-                if (!turn || !answer) {
-                    return CheckResult.wrong("The order of the steps in the game is broken 3.");
-                }
-                turn = false;
-                answer = false;
-                turnsNumber++;
-            }
+        int[] correctAnswer = grader(input, secretNumber);
+        int[] receivedAnswers = getNumOfBullsAndCows(output);
+
+        if (correctAnswer[0] != receivedAnswers[0]) {
+            return CheckResult.wrong("The number of Bulls is incorrect.");
         }
-        if (turnsNumber == 0) {
-            return CheckResult.wrong("The game log should contain at least two turns.");
-        }
-        if (!findFourDigitsWithRegExp(reply[reply.length - 1])) {
-            return CheckResult.wrong("The last string is incorrect.");
+
+        if (correctAnswer[1] != receivedAnswers[1]) {
+            return CheckResult.wrong("The number of Cows is incorrect.");
         }
 
         return CheckResult.correct();
     }
 
-    boolean findFourDigitsWithRegExp(String userString) {
+    Matcher getFourDigitsMatcher(String userString) {
         Pattern fourDigitsPattern = Pattern.compile("\\b\\d{4}\\b");
-        Matcher fourDigitsMatcher = fourDigitsPattern.matcher(userString);
-        return fourDigitsMatcher.find();
+        return fourDigitsPattern.matcher(userString);
+    }
 
+    boolean findFourDigitsWithRegExp(Matcher matcher) {
+        return matcher.find();
+    }
+
+    String getFourDigits(Matcher matcher) {
+        return matcher.group();
     }
 
     boolean findPairsOfBullsAndCows(String userString) {
-        Pattern pairPattern = Pattern.compile("(\\b\\d (cow|bull))|None\\b");
+        Pattern pairPattern = Pattern.compile("(\\b\\d ([cC]ow|[bB]ull))|[nN]one\\b");
         Matcher pairMatcher = pairPattern.matcher(userString);
         return pairMatcher.find();
+    }
+
+
+    // get number of bulls and cows from user program's output
+    int[] getNumOfBullsAndCows(String userString) {
+        Matcher nonePattern = Pattern.compile("\\b[nN]one\\b").matcher(userString);
+        Matcher cowsPattern = Pattern.compile("\\b\\d [cC]ow").matcher(userString);
+        Matcher bullsPattern = Pattern.compile("\\b\\d [bB]ull").matcher(userString);
+        Pattern oneNumPattern = Pattern.compile("\\d");
+
+        if (nonePattern.find()) {
+            return new int[]{0, 0};
+        }
+
+        int[] ans = {0, 0};
+
+        if (bullsPattern.find()) {
+            String temp = bullsPattern.group();
+            Matcher oneNumBulls = oneNumPattern.matcher(temp);
+            oneNumBulls.find();
+            ans[0] = Integer.parseInt(oneNumBulls.group());
+        }
+        if (cowsPattern.find()) {
+            String temp = cowsPattern.group();
+            Matcher oneNumCows = oneNumPattern.matcher(temp);
+            oneNumCows.find();
+            ans[1] = Integer.parseInt(oneNumCows.group());
+        }
+
+        return ans;
+    }
+
+    // reference grader
+    int[] grader(String grade, String guess) {
+        int bulls = 0;
+        List<Integer> gradeNumbers = stringToArrayOfNumbers(grade);
+        List<Integer> guessNumbers = stringToArrayOfNumbers(guess);
+
+        for (int i = 0; i < gradeNumbers.size(); i++) {
+            if (gradeNumbers.get(i).equals(guessNumbers.get(i))) {
+                bulls++;
+            }
+        }
+
+        gradeNumbers.retainAll(guessNumbers);
+        int cows = gradeNumbers.size() - bulls;
+
+        return new int[]{bulls, cows};
+    }
+
+
+    private static List<Integer> stringToArrayOfNumbers(String array) {
+        return Arrays.stream(array.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
