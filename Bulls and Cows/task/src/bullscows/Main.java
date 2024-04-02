8dd17716f0e874166dece67af3adc879df1ec2a2
Home <spirit40web@gmail.com>
2024-04-02 20:02:16 +0300

    Stage 1/7: Game log

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,17 @@
+package bullscows;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                The secret code is prepared: ****.
+                                
+                Turn 1. Answer:
+                1234
+                Grade: None.
+                                
+                Turn 2. Answer:
+                9876
+                Grade: 4 bulls.
+                Congrats! The secret code is 9876.""");
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+# Bulls and Cows
+
+## Project Overview
+**Platform:** Hyperskill  
+**Track:** Introduction to Java  
+**Project Link:** https://hyperskill.org/projects/53
+
+### About
+Write your own advanced version of the classic code breaking game "Bulls and Cows". Start with recreating the original version of the game and then take it to a new level.
+
+### Stages
+- Game log
+- Grader
+- Secret code
+- Game time
+- Improve the code generator
+- New level
+- Error
